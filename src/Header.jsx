79967d9ad4f339cb5e96c4ac6fe9af301ff640a8
Purv-8819 <purvpatel905@gmail.com>
2024-07-28 22:28:18 -0500

    Added Header and routing

---
+++ src/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Header.css";
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+
+  }
+
+  render() {
+    return (
+      <div id="header">
+       <h1 id="title">CREATORVERSE</h1>
+       <div>
+         <a href="/">
+            <button className="header-button">VIEW ALL CREATORS</button>
+         </a>
+         <a href="/add-creator">
+            <button className="header-button">ADD A CREATOR</button>
+         </a>
+       </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import App from './App.jsx'
+import Header from './Header.jsx';
 import './index.css'
+import AddForm from './add-form.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path:"head",
+        element: <Header />
+      }
+    ]
+  },
+  {
+    path: "add-creator",
+    element: <AddForm />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>test1</h1>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>
 )
